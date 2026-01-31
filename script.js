c419893028e2d95bb3c32e215c81766dd9e20196
SAURABH YADAV <saurabhsj069@gmail.com>
2026-01-31 23:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    // Allow only digits
+    if (!/^\d$/.test(value)) {
+      input.value = "";
+      return;
+    }
+
+    // Move forward
+    if (index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
+
+// Autofocus first box on load
+inputs[0].focus();
