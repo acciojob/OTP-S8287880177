2820def12b1c8cf8a2f6773ada9d1fe4f7129e75
SAURABH YADAV <saurabhsj069@gmail.com>
2026-01-31 23:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-const inputs = document.querySelectorAll(".code");
+const codes = document.querySelectorAll(".code");
 
-inputs.forEach((input, index) => {
+codes.forEach((input, index) => {
 
-  input.addEventListener("input", (e) => {
-    let val = e.target.value;
+  input.addEventListener("input", () => {
+    input.value = input.value.replace(/[^0-9]/g, "");
 
-    // Only allow digits
-    if (!/^[0-9]$/.test(val)) {
-      input.value = "";
-      return;
-    }
-
-    // Move forward automatically
-    if (index < inputs.length - 1) {
-      inputs[index + 1].focus();
+    if (input.value && index < codes.length - 1) {
+      codes[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      e.preventDefault(); // important for tests
-
-      // Always clear current box
-      input.value = "";
-
-      // Move focus backward AND clear that one too
-      if (index > 0) {
-        inputs[index - 1].value = "";
-        inputs[index - 1].focus();
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
       }
     }
   });
 
 });
 
-// Focus first box initially
-inputs[0].focus();
+// Focus first input on load
+codes[0].focus();
+
 
